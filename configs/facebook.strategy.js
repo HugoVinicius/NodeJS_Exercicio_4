5808ac36f2559aa87e7e4b45dc152ad92a0fd638
Hugo Vinicius <hugo.desenvolvimento@outlook.com>
2019-05-19 05:50:28 -0300

    removendo clientID e clientSecret orientação do github

--- configs/facebook.strategy.js
+++ configs/facebook.strategy.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new FacebookStrategy({
-    clientID: '2026214964174993',
-    clientSecret: 'f6cd887046108c0a75b7633e0fd2d832',
+    clientID: 'clientID',
+    clientSecret: 'clientSecret',
     callbackURL: "http://localhost:3000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
 
 passport.use(new GitHubStrategy({
-    clientID: 'Iv1.65a574fd8e733a13',
-    clientSecret: '6ceaad68deee094ce7d82806f7584e6bf65434bd',
+    clientID: 'clientID',
+    clientSecret: 'clientSecret',
     callbackURL: "http://localhost:3000/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
